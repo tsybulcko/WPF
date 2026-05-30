8e8d504ee370f2995854a369872d9c71f8f3bdb8
roman <roman@ROMANPC>
2026-05-31 00:52:11 +0300

    Binding

--- Binding/MainWindow.xaml.cs
+++ Binding/MainWindow.xaml.cs
@@ -10,19 +10,50 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
 
 namespace Binding
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : Window, INotifyPropertyChanged
 	{
 		public MainWindow()
 		{
+			DataContext = this;
 			InitializeComponent();
+		}
+		public event PropertyChangedEventHandler PropertyChanged;
+		string boundText;
+		public string BoundText
+		{
+			get => boundText;
+			set
+			{
+				boundText = value;
+				OnPropertyChanged();
+			}
+		}
+		void OnPropertyChanged([CallerMemberName]string propertyName = null)
+		{
+			PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			//PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(BoundText)));
+			//PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+			// ? (Null-conditional operator) - ипользуется при доступе к полям объекта или элементам массива.
+			// obj?.member - если (obj == null), оператор '?' вернет 'null', в противном случае вернет значение поля 'member'.
+
+			//htps://lwarn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-coalescing-operator
+		}
+
+		private void btnSet_Click(object sender, RoutedEventArgs e)
+		{
+			BoundText = "Default";
+			txtInput.Focus();
 		}
 	}
 }
--- Grid/MainWindow.xaml.cs
+++ Grid/MainWindow.xaml.cs
@@ -19,32 +19,12 @@ namespace Grid
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window, INotifyPropertyChanged
+	public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
-			DataContext = this;
 			InitializeComponent();
 		}
-		public event PropertyChangedEventHandler PropertyChanged;
-		string boundText;
-		public string BoundText
-		{
-			get => boundText;
-			set 
-			{  
-				boundText = value;
-				OnPropertyChanged();
-			}
-		}
-		void OnPropertyChanged()
-		{
-			PropertyChanged.Invoke(this, new PropertyChangedEventArgs("BoundText"));
-			//PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
-			// ? (Null-conditional operator) - ипользуется при доступе к полям объекта или элементам массива.
-			// obj?.member - если (obj == null), оператор '?' вернет 'null', в противном случае вернет значение поля 'member'.
-
-			//htps://lwarn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-coalescing-operator
-		}
+		
 	}
 }
